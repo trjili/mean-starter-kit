eea328a622e41c31faf03b8dd4b06f53eda06e85
trjili <trjili@galilee.fr>
2016-01-04 18:37:07 +0100

    [Server/user] password crypt

--- app/models/users.js
+++ app/models/users.js
@@ -1,8 +1,47 @@
 // nerd model
 var mongoose = require('mongoose');
+var bcrypt = require("bcryptjs");
 var Schema       = mongoose.Schema;
+var debug = require('debug');
 
-var usersSchema   = new Schema({
-    name: String
+var userSchema   = new Schema({
+    username: String,
+    password: String,
+    role: String,
+    email: String,
+    enabled: Boolean
 });
-module.exports = mongoose.model('user', usersSchema);
+
+// hash password for newest or updated user
+userSchema.pre('save', function(next) {
+   var user = this;
+    if (user.isModified('password') || user.isNew) {
+        bcrypt.genSalt(10, function(err, salt){
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function(err, hash){
+               if (err) {
+                   return next(err);
+               }
+               user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch){
+    if (err) {
+        debug(err);
+        cb(err);
+    }
+    debug('password match');
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('user', userSchema);
