8ac181e037949e9f2ecf3e5c692b85b8f35f98ce
Tarek Rjili <tarek.rjili@gmail.com>
2016-01-11 01:04:15 +0100

    [Client/Authentication] Authentication event constants

---
+++ public/js/config/Constants.js
@@ -0,0 +1,13 @@
+angular.module('meanStarterKit').constant('AUTH_EVENTS', {
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    tokenExpired: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+}).constant('USER_ROLES', {
+    all: '*',
+    admin: 'admin',
+    editor: 'editor',
+    guest: 'guest'
+});
--- public/js/services/HttpInterceptorService.js
+++ public/js/services/HttpInterceptorService.js
@@ -1,4 +1,4 @@
-angular.module('meanStarterKit').service('HttpInterceptorService', function($rootScope) {
+angular.module('meanStarterKit').service('HttpInterceptorService', function($rootScope, AUTH_EVENTS, $q) {
     var service = this;
 
     service.request = function(config) {
@@ -10,10 +10,14 @@ angular.module('meanStarterKit').service('HttpInterceptorService', function($roo
         return config;
     };
 
-    service.responseError = function(response) {
-        if (response.status === 401) {
-            $rootScope.$broadcast('unauthorized');
-        }
-        return response;
+    service.responseError = function (response) {
+        $rootScope.$broadcast({
+            401: AUTH_EVENTS.notAuthenticated,
+            403: AUTH_EVENTS.notAuthorized,
+            419: AUTH_EVENTS.tokenExpired,
+            440: AUTH_EVENTS.tokenExpired
+        }[response.status], response);
+
+        return $q.reject(response);
     }
 });
