0f2ba53ea87535926c2143d31a4d26694efbbf32
trjili <trjili@galilee.fr>
2016-01-12 19:12:48 +0100

    [Client/Users] Display users with angular-data-table module

--- public/js/controllers/UserController.js
+++ public/js/controllers/UserController.js
@@ -1,3 +1,27 @@
-angular.module('meanStarterKit').controller('UserController', function($scope){
-    $scope.tagLine = 'Nothing beats a pocket protector !';
-});
+angular.module('meanStarterKit').controller('UserController',['$scope', 'UserService', '$timeout', function($scope, UserService, $timeout){
+
+    $scope.selected = [];
+
+    $scope.query = {
+        order: 'name',
+        limit: 5,
+        page: 1
+    };
+    // init list
+    $scope.promise = getUsers();
+
+    function getUsers(query) {
+        return UserService.get().then(function(response){
+                $scope.users = response.data;
+        });
+    }
+
+    $scope.onPaginate = function (page, limit) {
+        $scope.promise = getUsers(angular.extend({}, $scope.query, {page: page, limit: limit}));
+    };
+
+    $scope.onReorder = function (order) {
+        $scope.promise = getUsers(angular.extend({}, $scope.query, {order: order}));
+
+    };
+}]);
